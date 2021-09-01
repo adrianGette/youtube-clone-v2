60e0fada13cc79780120abacad37accba4c09f30
Adrian Gette <adrian.gette@icloud.com>
2021-09-01 19:11:22 -0300

    Create HomeScreen component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
+import HomeScreen from './screens/homeScreen/HomeScreen'
 
 const App = () => {
     return (
-        <div>
-            Hola
-        </div>
+        <>
+            <Header />
+            <div className="app-container">
+                <Sidebar />
+                <Container fluid className="app-main">
+                    <HomeScreen />
+                </Container>
+            </div>
+        </>
     )
 }
 
