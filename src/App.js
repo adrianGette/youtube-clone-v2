509a6873dd9c9638c1d8b66efac8a32ca8da1f9c
Adrian Gette <adrian.gette@icloud.com>
2021-09-01 21:39:56 -0300

    Edit Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 
 import Header from './components/header/Header'
@@ -8,11 +8,16 @@ import HomeScreen from './screens/homeScreen/HomeScreen'
 import './_app.scss'
 
 const App = () => {
+
+    const [sidebar, toggleSidebar] = useState(false);
+
+    const handleToggleSidebar = () => toggleSidebar(value => !value);
+
     return (
         <>
-            <Header />
+            <Header handleToggleSidebar={handleToggleSidebar} />
             <div className="app__container border border-info">
-                <Sidebar />
+                <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
                 <Container fluid className="app__main border border-warning">
                     <HomeScreen />
                 </Container>
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,60 @@
 import React from 'react'
 import './_sidebar.scss'
 
-const Sidebar = () => {
+// import icons
+import {
+    MdSubscriptions,
+    MdExitToApp,
+    MdThumbUp,
+    MdHistory,
+    MdLibraryBooks,
+    MdHome,
+    MdSentimentDissatisfied
+} from 'react-icons/md'
+
+const Sidebar = ({ sidebar, handleToggleSidebar }) => {
     return (
-        <div className="border border-danger">
-            sidebar
-        </div>
+        <nav 
+            className={ sidebar ? "sidebar open" : "sidebar" }
+            onClick={ () => handleToggleSidebar(false) }
+        >
+            <li>
+                <MdHome size={23} />
+                <span>Principal</span>
+            </li>
+
+            <li>
+                <MdSubscriptions size={23} />
+                <span>Suscripciones</span>
+            </li>
+
+            <li>
+                <MdLibraryBooks size={23} />
+                <span>Biblioteca</span>
+            </li>
+
+            <li>
+                <MdHistory size={23} />
+                <span>Historial</span>
+            </li>
+
+            <li>
+                <MdThumbUp size={23} />
+                <span>Videos que me gustan</span>
+            </li>
+
+            {/* <li>
+                <MdSentimentDissatisfied size={23} />
+                <span>No me gusta</span>
+            </li> */}
+
+            <hr />
+            <li>
+                <MdExitToApp size={23} />
+                <span>Salir</span>
+            </li>
+            <hr />
+        </nav>
     )
 }
 
