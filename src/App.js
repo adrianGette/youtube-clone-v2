4bbed698a9aa6d490f2a1690a25c89d9cd5acc4b
Adrian Gette <adrian.gette@icloud.com>
2021-09-02 14:38:34 -0300

    Install and set up React Router

--- src/App.js
+++ src/App.js
@@ -6,26 +6,57 @@ import Sidebar from './components/sidebar/Sidebar'
 import HomeScreen from './screens/homeScreen/HomeScreen'
 import LoginScreen from './screens/loginScreen/LoginScreen'
 
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+
 import './_app.scss'
 
-const App = () => {
+const Layout = ({ children }) => {
 
     const [sidebar, toggleSidebar] = useState(false);
 
     const handleToggleSidebar = () => toggleSidebar(value => !value);
 
     return (
-        // <>
-        //     <Header handleToggleSidebar={handleToggleSidebar} />
-        //     <div className="app__container border border-info">
-        //         <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
-        //         <Container fluid className="app__main border border-warning">
-        //             <HomeScreen />
-        //         </Container>
-        //     </div>
-        // </>
+        <>
+            <Header handleToggleSidebar={handleToggleSidebar} />
+            <div className="app__container">
+                <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+                <Container fluid className="app__main">
+                    { children }
+                </Container>
+            </div>
+        </>
+    )
+}
 
-        <LoginScreen />
+const App = () => {
+
+    return (
+        <Router>
+            <Switch>
+            
+                <Route path="/" exact>
+                    <Layout>
+                        <HomeScreen />
+                    </Layout>
+                </Route>
+
+                <Route path="/auth">
+                    <LoginScreen />
+                </Route>
+
+                <Route path="/search">
+                    <Layout>
+                        <h1>Resultados de búsqueda</h1>
+                    </Layout>
+                </Route>
+
+                <Route>
+                    <Redirect to="/" />
+                </Route>
+
+            </Switch>
+        </Router>
     )
 }
 
