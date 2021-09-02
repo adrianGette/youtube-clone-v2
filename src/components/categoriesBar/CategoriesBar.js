19cb743c630cb756deb8fc230069d695c2aff754
Adrian Gette <adrian.gette@icloud.com>
2021-09-01 22:17:14 -0300

    Edit CategoriesBar component

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './_categoriesBar.scss'
 
+const keywords = [
+    'Todos',
+    'Programación',
+    'Patrones de diseño',
+    'Metodologías ágiles',
+    'Música',
+    'Fútbol',
+    'Celulares',
+    'Laptops',
+    'Recetas de cocina',
+    'Tendencias de música urbana',
+    'En vivo',
+    'Noticias',
+    'Fútbol argentino',
+    'Criptomonedas',
+    'Billeteras digitales',
+    'Bases de datos',
+    'Punk rock music'
+]
+
 const CategoriesBar = () => {
+
+    const [ activeElement, setActiveElement ] = useState('Todos');
+
+    const handleClick = (value) => {
+        setActiveElement(value);
+    }
+
     return (
-        <div>
-            CategoriesBar
+        <div className="categoriesBar">
+            { keywords.map( (value, i) => (
+                <span 
+                    key={i}
+                    onClick={ () => handleClick(value) }
+                    className={ activeElement === value ? 'active' : '' }
+                >{value}</span>) 
+            ) }
         </div>
     )
 }
