38cae0627e10abd52cc2f49f9038ec2030e3fa0a
Adrian Gette <adrian.gette@icloud.com>
2021-09-05 15:44:43 -0300

    Show data from Youtube to HomeScreen

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,27 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './_video.scss'
 
 import { AiFillEye } from 'react-icons/ai'
+import request from '../../api'
+
+import moment from 'moment'
+import numeral from 'numeral'
 
 const Video = ({video}) => {
+
+    const {
+        id,
+        snippet: {
+            channelId,
+            channelTitle,
+            title,
+            publishedAt,
+            thumbnails: { medium },
+        },
+    } = video;
+
+    const [ views, setViews ] = useState(null);
+    const [ duration, setDuration ] = useState(null);
+    const [ channelIcon, setChannelIcon ] = useState(null);
+
+    const seconds = moment.duration(duration).asSeconds();
+    const _duration = moment.utc(seconds * 1000).format("mm:ss");
+
+
+    useEffect(() => {
+        const get_video_details = async () => {
+            const {data:{items}} = await request('/videos', {
+                params: {
+                    part: 'contentDetails,statistics',
+                    id: id,
+                },
+            })
+            setDuration(items[0].contentDetails.duration);
+            setViews(items[0].statistics.viewCount);
+        }
+        get_video_details();
+    }, [id])
+
+    useEffect(() => {
+        const get_channel_icon = async () => {
+            const {data:{items},} = await request('/channels', {
+                params: {
+                    part: 'snippet',
+                    id: channelId,
+                },
+            })
+            setChannelIcon(items[0].snippet.thumbnails.default)
+        }
+        get_channel_icon();
+    }, [channelId])
+
     return (
         <div className="video">
             <div className="video__top">
-                <img src="https://i.ytimg.com/vi/xXE4tfrPB2A/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&amp;rs=AOn4CLBfCVJ3znPobXeQ3l_Jfg3W1BeVyA" alt="" />
-                <span>07:30</span>
+                <img src={medium.url} alt="" />
+                <span>{_duration}</span>
             </div>
             <div className="video__title">
-                Tendencias Música 2021
+                {title}
             </div>
             <div className="video__details">
                 <span>
-                    <AiFillEye /> 15m vistas • 
+                    <AiFillEye /> {numeral(views).format("0.a")} vistas • 
                 </span>
-                <span> hace 10 días</span>
+                <span>{moment(publishedAt).fromNow()}</span>
             </div>
             <div className="video__channel">
-                <img src="https://yt3.ggpht.com/PovCcIPhkm7pfbiC2gW4WfumIzxbFMmVlW6RAZAs_txFtru5jm5_JHE-rXpgyd8E356Mt3nn=s88-c-k-c0x00ffffff-no-rj" alt="" />
-                <p>Music For All</p>
+                <img src={channelIcon?.url} alt="" />
+                <p>{channelTitle}</p>
             </div>
         </div>
     )
