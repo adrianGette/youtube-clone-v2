045bc771650c917e296eb6c1225ca249c7b532c1
Adrian Gette <adrian.gette@icloud.com>
2021-09-08 15:56:26 -0300

    Add Skeleton component in HomeScreen

--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -1,54 +1,64 @@
-import React, { useEffect } from 'react'
-import { Col, Container } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from "react";
+import { Col, Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import CategoriesBar from '../../components/categoriesBar/CategoriesBar'
-import Video from '../../components/video/Video'
-import { getPopularVideos, getVideosByCategory } from '../../redux/actions/videos.action'
+import CategoriesBar from "../../components/categoriesBar/CategoriesBar";
+import Video from "../../components/video/Video";
+import {
+  getPopularVideos,
+  getVideosByCategory,
+} from "../../redux/actions/videos.action";
 
-import InfiniteScroll from 'react-infinite-scroll-component'
+import InfiniteScroll from "react-infinite-scroll-component";
+import SkeletonVideo from "../../skeletons/SkeletonVideo";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPopularVideos());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getPopularVideos());
-    }, [dispatch]);
+  const { videos, activeCategory, loading } = useSelector((state) => state.homeVideos);
 
-    const { videos, activeCategory } = useSelector(state => state.homeVideos);
-
-    const fetchData = () => {
-        if(activeCategory === "All") {
-            dispatch(getPopularVideos());
-        } else {
-            dispatch(getVideosByCategory(activeCategory));
-        }
-        
+  const fetchData = () => {
+    if (activeCategory === "All") {
+      dispatch(getPopularVideos());
+    } else {
+      dispatch(getVideosByCategory(activeCategory));
     }
+  };
 
-    return (
-        <Container>
-            <CategoriesBar />
-           
-                <InfiniteScroll
-                    dataLength={videos.length}
-                    next={fetchData}
-                    hasMore={true}
-                    loader={
-                        <div className="spinner-border text-danger d-block mx-auto"></div>
-                    }
-                className="row">
-                    { videos.map( video => (
-                        <Col lg={3} md={4} key={video.id}>
-                            <Video video={video} />
-                        </Col>
-                    ) )}
-                </InfiniteScroll>
-          
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <CategoriesBar />
 
-export default HomeScreen
+      <InfiniteScroll
+        dataLength={videos.length}
+        next={fetchData}
+        hasMore={true}
+        loader={
+          <div className="spinner-border text-danger d-block mx-auto"></div>
+        }
+        className="row"
+      >
+        {!loading ? (videos.map((video) => (
+          <Col lg={3} md={4} key={video.id}>
+            <Video video={video} />
+          </Col>
+        )))
+        : 
+            [...Array(20)].map(() => (
+                <Col lg={3} md={4}>
+                    <SkeletonVideo />
+                </Col>
+            ))
+        
+        }
+      </InfiniteScroll>
+    </Container>
+  );
+};
+
+export default HomeScreen;
