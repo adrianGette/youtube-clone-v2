2d02631b8124a41823b80b56fe82c8d9cc214e85
Adrian Gette <adrian.gette@icloud.com>
2021-09-10 18:23:51 -0300

    Add structure from single Comment component

---
+++ src/components/comment/Comment.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import moment from 'moment'
+
+const Comment = () => {
+    return (
+        <div className="comment p-2 d-flex">
+            <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd_Ea7Wyc7-XNssIk4eSNx0IBR5l4wZnWKhw&usqp=CAU"
+                alt="Imagen de perfil"
+                className="rounded-circle mr-3" 
+            />
+            <div className="comment__body">
+                <p className="comment__header">
+                    Adrian Gette •  {moment("2021-09-10").fromNow()}
+                </p>
+                <p>Excelente video!!!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Comment
--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import './_comments.scss'
+import Comment from '../comment/Comment'
+
+
+const handleComment = () => {
+
+}
 
 const Comments = () => {
     return (
-        <div>
-            comentarios
+        <div className="comments">
+            <p>503 comentarios</p>
+            <div className="comment__form d-flex w-100 my-2">
+                <img 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd_Ea7Wyc7-XNssIk4eSNx0IBR5l4wZnWKhw&usqp=CAU"
+                    alt="Imagen de perfil"
+                    className="rounded-circle mr-3" 
+                />
+                <form onSubmit={handleComment} className="d-flex flex-grow-1">
+                    <input 
+                        type="text"
+                        className="flex-grow-1"
+                        placeholder="Escribe un comentario. . ."
+                    />
+                    <button className="border-0 p-2">Comentar</button>
+                </form>
+            </div>
+            <div className="comments__list">
+                {
+                    [...Array(15)].map(() => (
+                        <Comment />
+                    ))
+                }
+            </div>
         </div>
     )
 }
