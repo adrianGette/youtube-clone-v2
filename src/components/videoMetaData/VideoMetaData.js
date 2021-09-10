128b973b923dd7c700898597f4a6523b8103a401
Adrian Gette <adrian.gette@icloud.com>
2021-09-10 17:39:51 -0300

    Add some styles to VideoMetaData component

--- src/components/videoMetaData/VideoMetaData.js
+++ src/components/videoMetaData/VideoMetaData.js
@@ -3,6 +3,7 @@ import './_videoMetaData.scss'
 import moment from 'moment'
 import numeral from 'numeral'
 import { MdThumbUp, MdThumbDown } from 'react-icons/md'
+import ReactShowMoreText from 'react-show-more-text'
 
 const VideoMetaData = () => {
     return (
@@ -16,10 +17,10 @@ const VideoMetaData = () => {
                     </span>
                 
                 <div>
-                    <span className="mr-3">
+                    <span className="mx-3">
                         <MdThumbUp size={26} /> {numeral(1000).format("0.a")}
                     </span>
-                    <span className="mr-3">
+                    <span className="mx-3">
                         <MdThumbDown size={26} /> {numeral(1000).format("0.a")}
                     </span>
                 </div>
@@ -30,7 +31,7 @@ const VideoMetaData = () => {
                     <img 
                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd_Ea7Wyc7-XNssIk4eSNx0IBR5l4wZnWKhw&usqp=CAU"
                         alt="Imagen de perfil" 
-                        className="rounded-circle mr3"
+                        className="rounded-circle mx-3"
                     />
                     <div className="d-flex flex-column">
                         <span>Nombre del canal</span>
@@ -40,7 +41,16 @@ const VideoMetaData = () => {
                 <button className="btn border-0 p-2 m-2">Suscribirse</button>
             </div>
             <div className="videoMetaData__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vestibulum dignissim enim, id varius nunc ullamcorper vel. Mauris convallis molestie nisi, non tincidunt metus convallis a. Nam et eros augue. Integer maximus ex a laoreet rutrum. Proin vehicula dapibus neque. Etiam euismod leo non nisi vestibulum, vitae interdum lectus auctor.
+                <ReactShowMoreText
+                    lines={3}
+                    more="Mostrar Más"
+                    less="Mostrar Menos"
+                    anchorClass="showMoreText"
+                    expanded={false}
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vestibulum dignissim enim, id varius nunc ullamcorper vel. Mauris convallis molestie nisi, non tincidunt metus convallis a. Nam et eros augue. Integer maximus ex a laoreet rutrum. Proin vehicula dapibus neque. Etiam euismod leo non nisi vestibulum, vitae interdum lectus auctor.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vestibulum dignissim enim, id varius nunc ullamcorper vel. Mauris convallis molestie nisi, non tincidunt metus convallis a. Nam et eros augue. Integer maximus ex a laoreet rutrum. Proin vehicula dapibus neque. Etiam euismod leo non nisi vestibulum, vitae interdum lectus auctor.
+                </ReactShowMoreText>
             </div>
         </div>
     )
